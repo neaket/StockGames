13620b97a89645c0b57ba727898049f424e94ba4
Jonathon Panke <laxaues@gmial.com>
2012-10-19 15:08:05 -0400

    The bare bones to the communication module, no actual code, thats to be implemented in the future.

---
+++ StockGamesWP7/CommunicationProtocol/MessageQueue.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace StockGames.CommunicationProtocol
+{
+    public class MessageQueue
+    {
+
+        public MessageQueue()
+        {
+        }
+
+        public void sendServerRequest()
+        {
+        }
+
+        public void getServerRequest()
+        {
+        }
+
+        public void pop()
+        {
+        }
+
+        public void push()
+        {
+        }
+    }
+}
