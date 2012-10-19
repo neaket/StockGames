656e20415cc30c3815dd9f038b243171281b69ae
Nick Eaket <neaket@gmail.com>
2012-10-19 17:13:26 -0400

    Added StockView and StockViewModel.

---
+++ StockGamesWP7/ViewModels/StockViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace StockGames.ViewModels
+{
+    public class StockViewModel
+    {
+    //    Stock Stock;
+
+    //    public StockViewModel()
+    //    {
+    //        // Temp Code
+    //        Stock = new Stock() {
+    //           CurrentMarketPrice = 35.33M,
+    //           Symbol = "ABC",
+    //           Company = "ABC Company",
+    //           High = 36M,
+    //           Low = 35M,
+    //           MarketCap = 350000M,
+    //           Open = 35.5M,
+    //           ProfitAndLoss = -1.01M      
+               
+    //        };
+    //    }
+    }
+}
