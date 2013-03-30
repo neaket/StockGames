1ab203e98de3c87ca5cb335c6e51a04753934bb9
Nick Eaket <neaket@gmail.com>
2013-03-30 11:33:14 -0400

    Added a sample implementation to show how to use a message to signal when the communication is completed.

---
+++ StockGamesWP7/Messaging/CommunicationStateChangedMessageType.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using StockGames.CommunicationModule;
+
+namespace StockGames.Messaging
+{
+    public class CommunicationStateChangedMessageType
+    {
+        public ProcessState CurrentState { get; private set; }
+
+        public CommunicationStateChangedMessageType(ProcessState currentState) 
+        {
+            CurrentState = currentState;
+        }
+    }
+}
